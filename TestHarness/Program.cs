8b25172c19d2c1e13d1083fb762f1c9c4b4b00d7
Bradley Morris <bradley.morris@aaon.com>
2021-10-12 18:19:36 -0500

    feat(BDMJsonProcs): add method to accept object input for no response.

--- TestHarness/Program.cs
+++ TestHarness/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BDMJsonProcs;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace TestHarness
 {
@@ -6,6 +10,16 @@ namespace TestHarness
 	{
 		static void Main(string[] args)
 		{
+			String connectionString = "Server=localhost;Initial Catalog=BDMSQLUtilities;User Id=BDMSQLUtilities.WebAPI;Password=213SDfg34sdabghfd&##dsfgsdfg^;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"BDMSQLUtilities.WebAPI\";";
+
+			using (SqlCommand sqlCommand = new())
+			{
+				var sdfasdf = sqlCommand.ExecuteScalarJsonProc<List<String>>(
+					$"[Optimization].[GetMissingIndexStatistics.json]",
+					connectionString,
+					new SqlParameter() { ParameterName = "@DatabaseName", SqlDbType = SqlDbType.NVarChar, Size = 128, SqlValue = "TrashDB", Direction = ParameterDirection.Input }
+				);
+			}
 			Console.WriteLine("Hello World!");
 		}
 	}
