e6e8702f02b1acae2e89e2e5d85230e6b5373b1a
Bradley Don Morris <bradleydonmorris@hotmail.com>
2022-06-14 01:23:56 -0500

    feat: expand BDMJsonProcs to handle paramters on JSON

--- BDMJsonProcs/SqlCommandExtensions.cs
+++ BDMJsonProcs/SqlCommandExtensions.cs
@@ -1,4 +1,5 @@
 ﻿ using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using Microsoft.Data.SqlClient;
@@ -8,84 +9,6 @@ namespace BDMJsonProcs
 	// TODO: Build better XML comments
 	public static class SqlCommandExtensions
 	{
-		#region No Response
-		///<summary>
-		/// Example Usage:
-		///		using (SqlCommand sqlCommand = new())
-		///			sqlCommand.ExecuteNoResponse("[Widgets].[Delete]", this._ConnectionString,
-		///				new SqlParameter { ParameterName = "WidgetGUID", SqlDbType = SqlDbType.UniqueIdentifier, Value = guid }
-		///			);
-		/// </summary>
-		/// <param name="command"></param>
-		/// <param name="commandText"></param>
-		/// <param name="connectionString"></param>
-		/// <param name="parameters"></param>
-		public static void ExecuteNoResponse(
-			this SqlCommand command,
-			String commandText,
-			String connectionString,
-			params SqlParameter[] parameters)
-		{
-			command.Connection = new SqlConnection(connectionString);
-			command.CommandType = CommandType.StoredProcedure;
-			command.CommandText = commandText;
-			foreach (SqlParameter parameter in parameters)
-				command.Parameters.Add(parameter);
-			if (command.Connection.State != ConnectionState.Open)
-				command.Connection.Open();
-			command.ExecuteNonQuery();
-			if (command.Connection.State != ConnectionState.Closed)
-				command.Connection.Close();
-			command.Connection.Dispose();
-		}
-
-		/// <summary>
-		/// Example Usage:
-		///		using (SqlCommand sqlCommand = new())
-		///			return sqlCommand.ExecuteNoResponse("[Widgets].[FindWidgets]", this._ConnectionString,
-		///											new WidgetSearchOptions()
-		///											{
-		///												NameLike = "Cool%",
-		///												DescriptionLike = "%red%",
-		///												Type = "Bicycle"
-		///											},
-		///											"MyJSONStringParameters"
-		///										);
-		/// </summary>
-		/// <param name="command"></param>
-		/// <param name="commandText"></param>
-		/// <param name="connectionString"></param>
-		/// <param name="inputObject"></param>
-		/// <param name="parameterName"></param>
-		public static void ExecuteNoResponse(
-			this SqlCommand command,
-			String commandText,
-			String connectionString,
-			Object inputObject,
-			String parameterName = "JSON")
-		{
-			command.Connection = new SqlConnection(connectionString);
-			command.CommandType = CommandType.StoredProcedure;
-			command.CommandText = commandText;
-			command.Parameters.Add(new SqlParameter
-			{
-				ParameterName = parameterName,
-				SqlDbType = SqlDbType.NVarChar,
-				Size = (-1),
-				Value = Newtonsoft.Json.JsonConvert.SerializeObject(inputObject)
-			});
-			if (command.Connection.State != ConnectionState.Open)
-				command.Connection.Open();
-			if (command.Connection.State != ConnectionState.Open)
-				command.Connection.Open();
-			command.ExecuteNonQuery();
-			if (command.Connection.State != ConnectionState.Closed)
-				command.Connection.Close();
-			command.Connection.Dispose();
-		}
-		#endregion No Response
-
-
 		#region Return desialized Object
 		/// <summary>
 		/// Example Usage:
@@ -181,6 +104,47 @@ namespace BDMJsonProcs
 				returnValue = Newtonsoft.Json.JsonConvert.DeserializeObject<TResult>(scalarResult);
 			return returnValue;
 		}
+
+		/// <summary>
+		/// Example Usage:
+		///		using (SqlCommand sqlCommand = new())
+		///			return sqlCommand.ExecuteScalarJsonProc<IEnumerable<Widget>>("[Widgets].[FindWidgets]", this._ConnectionString,
+		///											new WidgetSearchOptions()
+		///											{
+		///												NameLike = "Cool%",
+		///												DescriptionLike = "%red%",
+		///												Type = "Bicycle"
+		///											});
+		/// </summary>
+		/// <typeparam name="TResult"></typeparam>
+		/// <param name="command"></param>
+		/// <param name="commandText"></param>
+		/// <param name="connectionString"></param>
+		/// <param name="inputObject"></param>
+		/// <param name="parameterName"></param>
+		/// <param name="parameters"></param>
+		/// <returns>TResult deserialized from JSON</returns>
+		public static TResult? ExecuteScalarJsonProc<TResult>(
+			this SqlCommand command,
+			String commandText,
+			String connectionString,
+			Object inputObject,
+			String parameterName = "JSON",
+			params SqlParameter[] parameters)
+			where TResult : class, new()
+		{
+			TResult? returnValue = null;
+			String? scalarResult = command.ExecuteScalarJsonProc(
+				commandText,
+				connectionString,
+				inputObject,
+				parameterName,
+				parameters
+			);
+			if (!String.IsNullOrWhiteSpace(scalarResult))
+				returnValue = Newtonsoft.Json.JsonConvert.DeserializeObject<TResult>(scalarResult);
+			return returnValue;
+		}
 		#endregion Return desialized Object
 
 		#region Return String
@@ -259,9 +223,173 @@ namespace BDMJsonProcs
 					Value = Newtonsoft.Json.JsonConvert.SerializeObject(inputObject)
 				}
 			);
+
+		/// <summary>
+		/// Example Usage:
+		///		using (SqlCommand sqlCommand = new())
+		///			return sqlCommand.ExecuteScalarJsonProc("[Widgets].[FindWidgets]", this._ConnectionString,
+		///									new WidgetSearchOptions()
+		///									{
+		///										NameLike = "Cool%",
+		///										DescriptionLike = "%red%",
+		///										Type = "Bicycle"
+		///									},
+		///									new SqlParameter { ParameterName = "WidgetGUID", SqlDbType = SqlDbType.UniqueIdentifier, Value = guid }
+		///								);
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="commandText"></param>
+		/// <param name="connectionString"></param>
+		/// <param name="inputObject"></param>
+		/// <param name="parameterName"></param>
+		/// <param name="parameters"></param>
+		/// <returns>JSON String</returns>
+		public static String? ExecuteScalarJsonProc(
+			this SqlCommand command,
+			String commandText,
+			String connectionString,
+			Object inputObject,
+			String parameterName = "JSON",
+			params SqlParameter[] parameters)
+		{
+			List<SqlParameter> parametersList = new(parameters);
+			parametersList.Add(
+				new SqlParameter
+				{
+					ParameterName = parameterName,
+					SqlDbType = SqlDbType.NVarChar,
+					Size = (-1),
+					Value = Newtonsoft.Json.JsonConvert.SerializeObject(inputObject)
+				}
+			);
+			return command.ExecuteScalarString(
+				commandText,
+				connectionString,
+				parametersList.ToArray()
+			);
+		}
 		#endregion Return String 
 
-		#region SqlCommand.ExecuteScalar Replacement
+		#region Return void
+		/// <summary>
+		/// Example Usage:
+		///		using (SqlCommand sqlCommand = new())
+		///			sqlCommand.ExecuteJsonProcNoResults<IEnumerable<Widget>>("[Widgets].[Post]", this._ConnectionString);
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="commandText"></param>
+		/// <param name="connectionString"></param>
+		public static void ExecuteJsonProcNoResults(
+			this SqlCommand command,
+			String commandText,
+			String connectionString)
+			=> command.ExecuteNoResults(
+				commandText,
+				connectionString
+			);
+
+		/// <summary>
+		/// Example Usage:
+		///		using (SqlCommand sqlCommand = new())
+		///			(sqlCommand.ExecuteJsonProcNoResults("[Widgets].[Post]", this._ConnectionString,
+		///				new SqlParameter { ParameterName = "WidgetGUID", SqlDbType = SqlDbType.UniqueIdentifier, Value = guid }
+		///			);
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="commandText"></param>
+		/// <param name="connectionString"></param>
+		/// <param name="parameters"></param>
+		public static void ExecuteJsonProcNoResults(
+			this SqlCommand command,
+			String commandText,
+			String connectionString,
+			params SqlParameter[] parameters)
+			=> command.ExecuteNoResults(
+				commandText,
+				connectionString,
+				parameters
+			);
+
+		/// <summary>
+		/// Example Usage:
+		///		using (SqlCommand sqlCommand = new())
+		///			sqlCommand.ExecuteJsonProcNoResults("[Widgets].[Post]", this._ConnectionString,
+		///											new WidgetSearchOptions()
+		///											{
+		///												NameLike = "Cool%",
+		///												DescriptionLike = "%red%",
+		///												Type = "Bicycle"
+		///											});
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="commandText"></param>
+		/// <param name="connectionString"></param>
+		/// <param name="inputObject"></param>
+		/// <param name="parameterName"></param>
+		public static void ExecuteJsonProcNoResults(
+			this SqlCommand command,
+			String commandText,
+			String connectionString,
+			Object inputObject,
+			String parameterName = "JSON")
+			=> command.ExecuteNoResults(
+				commandText,
+				connectionString,
+				new SqlParameter
+				{
+					ParameterName = parameterName,
+					SqlDbType = SqlDbType.NVarChar,
+					Size = (-1),
+					Value = Newtonsoft.Json.JsonConvert.SerializeObject(inputObject)
+				}
+			);
+
+		/// <summary>
+		/// Example Usage:
+		///		using (SqlCommand sqlCommand = new())
+		///			sqlCommand.ExecuteJsonProcNoResults("[Widgets].[Post]", this._ConnectionString,
+		///									new WidgetSearchOptions()
+		///									{
+		///										NameLike = "Cool%",
+		///										DescriptionLike = "%red%",
+		///										Type = "Bicycle"
+		///									},
+		///									new SqlParameter { ParameterName = "WidgetGUID", SqlDbType = SqlDbType.UniqueIdentifier, Value = guid }
+		///								);
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="commandText"></param>
+		/// <param name="connectionString"></param>
+		/// <param name="inputObject"></param>
+		/// <param name="parameterName"></param>
+		/// <param name="parameters"></param>
+		public static void ExecuteJsonProcNoResults(
+			this SqlCommand command,
+			String commandText,
+			String connectionString,
+			Object inputObject,
+			String parameterName = "JSON",
+			params SqlParameter[] parameters)
+		{
+			List<SqlParameter> parametersList = new(parameters);
+			parametersList.Add(
+				new SqlParameter
+				{
+					ParameterName = parameterName,
+					SqlDbType = SqlDbType.NVarChar,
+					Size = (-1),
+					Value = Newtonsoft.Json.JsonConvert.SerializeObject(inputObject)
+				}
+			);
+			command.ExecuteNoResults(
+				commandText,
+				connectionString,
+				parametersList.ToArray()
+			);
+		}
+		#endregion Return void
+
+		#region SqlCommand.Execute{xxx} Replacement
 		/// <summary>
 		/// Used to overcome limitations of SqlCommand.ExecuteScalar
 		/// </summary>
@@ -320,6 +448,54 @@ namespace BDMJsonProcs
 			command.Connection.Dispose();
 			return returnValue;
 		}
-		#endregion SqlCommand.ExecuteScalar Replacement
+
+		/// <summary>
+		/// Used to overcome limitations of SqlCommand.ExecuteScalar
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="commandText"></param>
+		/// <param name="connectionString"></param>
+		public static void ExecuteNoResults(
+			this SqlCommand command,
+			String commandText,
+			String connectionString)
+		{
+			command.Connection = new SqlConnection(connectionString);
+			command.CommandType = CommandType.StoredProcedure;
+			command.CommandText = commandText;
+			if (command.Connection.State != ConnectionState.Open)
+				command.Connection.Open();
+			command.ExecuteNonQuery();
+			if (command.Connection.State != ConnectionState.Closed)
+				command.Connection.Close();
+			command.Connection.Dispose();
+		}
+
+		/// <summary>
+		/// Wrapper for ExecuteNonQuery
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="commandText"></param>
+		/// <param name="connectionString"></param>
+		/// <param name="parameters"></param>
+		public static void ExecuteNoResults(
+			this SqlCommand command,
+			String commandText,
+			String connectionString,
+			params SqlParameter[] parameters)
+		{
+			command.Connection = new SqlConnection(connectionString);
+			command.CommandType = CommandType.StoredProcedure;
+			command.CommandText = commandText;
+			foreach (SqlParameter parameter in parameters)
+				command.Parameters.Add(parameter);
+			if (command.Connection.State != ConnectionState.Open)
+				command.Connection.Open();
+			command.ExecuteNonQuery();
+			if (command.Connection.State != ConnectionState.Closed)
+				command.Connection.Close();
+			command.Connection.Dispose();
+		}
+		#endregion SqlCommand.Execute{xxx} Replacement
 	}
 }
--- TestHarness/Program.cs
+++ TestHarness/Program.cs
@@ -18,121 +18,21 @@ namespace TestHarness
 	{
 		static void Main() //(string[] args)
 		{
-			//Guid asdf = default;
-			Provider provider1 = new()
-			{
-				KeyGUID = Guid.NewGuid(),
-				Name = "Me"
-			};
-			Provider provider2 = new()
-			{
-				KeyGUID = provider1.KeyGUID,
-				Name = "Me"
-			};
-
-			Person person1 = new()
-			{
-				KeyGUID = Guid.NewGuid(),
-				FullName = "Bradley Don Morris",
-				FirstName = "Bradley",
-				LastName = "Morris"
-			};
-			Person person2 = new()
-			{
-				KeyGUID = Guid.NewGuid(),
-				FullName = "Bradley Don Morris",
-				FirstName = "Bradley",
-				LastName = "Morris"
-			};
-			var asdfsadf = provider1.AreKeysEqual(provider2);
-			var zsdf = person1.AreKeysEqual(person2);
-			var zsdfas = person1.AreKeysEqual(person2, "Name");
-
-			SARTeam.Security.DeviceToken dt1 = new("bradleydonmorris@sar.team", "HP-User", "ThisComputer", "It (1.0.0.0)");
-			String temp = dt1.Seal().Serialize(true, true);
-			//String teer = "{\"State\": \"Sealed\",\"StateComment\": \"\",\"Hash\": \"zPuiDj8V4vvtQUp4mGI2SdqkbEUW4kDY/1b+x/nxj7KB7a0cBQhbEct6/2RSUjtrx/VKdDeN5kycfV79KbrimOJ0T1arIe+ttfcR1HXrzyQrN8OuqTbcVm6OkfwEgzIpb+6g54p5kKc0PK5Xuu2iVs1H+cHz/14oi15Xjc+Bcw3sKIje3zDO0OhQWSwBcMfQ54utEq2ailbn2u+gj8sJXqsWZ8xltxUAtTiHCb53ZwEmDap4mYBQ6dZk2kVv8YztHeviZVEeJ+cljYrB+ImfAs/0TANSjOtUknGOdJ7dS+3OtinBI7orczQchRsbdOKWiesZZMToULKPfHVo300YLg==\",\"Content\": \"ksPyz/8GSByz7XSA3Tv9BccojVCie3TBrXZTtMWUsvDhrmn/k8rZJjRb0MK2ANbFZVxZJYlCdMhq4vP292gRElDPCsG8Gq/SEwi7twa2E8yo96W29M6dW+YSM3veQQdR3oGlTsk3HrU9e3rh7adfCxxasL9tFB01OEQiOO4ZKUtGWt2BjgkDY/tgXG78WHCvwNXkh/1LFHjS8sljnfirMEy8aIGfLuCaDXRoN9m3HVCF7I7/ZHgIJH3ODGq+U/FIVaFhruxY2DgbAeSAje8us9rNK2G16AMdlDaTh9Tpcpzuh6tp2hJhRiCYAxdZQsLL\",\"Passphrase\": \"YwwmMw1lZV3UOI6TPruuu0frPo2j0sQDH87B9lOUcC2lnTzsS/IFvecC+d9sx4ocGRk/bofjLstZVtLagxikC0xMXAJ6GZKbJH+Fw4zIrzIUfj74itPmYSfvjcIfbf8dSvl4fYFvu5amUptq17Hz7WgJSpLbIv8A8qichyu22NvJGAu8q81NKLGIkbJTNHZ5dzci2z9It9/INNdOqBT35AuLItCGuVIwxgb6lJIhX3FefnDqv1aJgv0yB7QLnPLDnNMqiF6SB8AKmAF5QxMeVocmXFnmXz1ECT7Zt5l559+lo+BUO8B6oYHAN8Pz8niJht+K7msAMRW8Bnl6B+UHRg==\"}";
-			SARTeam.Security.DeviceToken? dt = SARTeam.Security.DeviceToken.Open(BDMCiphers.Envelope.Deserialize(temp, true));
-
-			String[] replacementSet = LetterSubsituteSets.CreateNewSet("ABCDEF1234567890");
- 			String replacements = LetterSubsituteSets.JoinSet(replacementSet);
-			LetterSubsituteSets.ParseSet(replacements);
-
-			String entity = LetterSubsituteSets.GetRandom2Bytes(replacementSet);
-			String system = LetterSubsituteSets.GetRandom2Bytes(replacementSet);
-
-			LetterSubstituteIdentifier l = new(replacementSet, entity, system);
-			Console.WriteLine($"replacements     {replacements}");
-			Console.WriteLine($"Contacts         {LetterSubsituteSets.GetRandom2Bytes(replacementSet)}");
-			Console.WriteLine($"Events           {LetterSubsituteSets.GetRandom2Bytes(replacementSet)}");
-			Console.WriteLine($"Resources        {LetterSubsituteSets.GetRandom2Bytes(replacementSet)}");
-			Console.WriteLine($"Other            {LetterSubsituteSets.GetRandom2Bytes(replacementSet)}");
-			Console.WriteLine($"Bradley Laptop   {LetterSubsituteSets.GetRandom2Bytes(replacementSet)}");
-
-
-			String TheString = "ybseybseybybybse";
-			String seg1 = TheString[..4];
-			String seg2 = TheString[4..8];
-			String seg3 = TheString[^8..];
-
-			Console.WriteLine($"seg1        {seg1}");
-			Console.WriteLine($"seg1[..2]   {seg1[..2]}");
-			Console.WriteLine($"seg1[^2..]  {seg1[^2..]}");
-			Console.WriteLine("");
-			Console.WriteLine($"seg2        {seg2}");
-			Console.WriteLine($"seg2[..2]   {seg2[..2]}");
-			Console.WriteLine($"seg2[^2..]  {seg2[^2..]}");
-
-			Console.WriteLine("");
-			Console.WriteLine($"seg3        {seg3}");
-			Console.WriteLine($"seg3[..2]   {seg3[..2]}");
-			Console.WriteLine($"seg3[2..4]   {seg3[2..4]}");
-			Console.WriteLine($"seg3[4..6]   {seg3[4..6]}");
-			Console.WriteLine($"seg3[^2..]  {seg3[^2..]}");
-
-			//BDMEntityNumbering.SequentialIdentifier s = new("ABC0001000200000003XYZ");
-			//String four = s.GetNext();
-			//String five = s.GetNext();
-
-
-			BDMEntityNumbering.LetterSubstituteIdentifier s2 = new(String.Empty, String.Empty, "ybseybseybybybse");
-			String eight = s2.Increment();
-
-			//BDMCommandLine.ICommand 
-
-			Log.Logger = new LoggerConfiguration()
-				.WriteTo.BDMSerilogProc(
-					"Server=localhost;Initial Catalog=AAONEnterprise;User Id=AAONEnterprise.Logger;Password=cf$g0f3FMxvkuwedSD#@IbHd162Vrm*5#SE;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=SerilogPoC;",
-					"Logging",
-					"Enqueue",
-					"JSON",
-					LogEventLevel.Verbose
-				).CreateLogger();
-			try
-			{
-				throw new Exception("Gettin it goin.");
-			}
-			catch (Exception exception)
-			{
-				Log.Logger.Error(exception, "{CallingMethod}", new CallingMethod()
+			String connectionString = "Server=tcp:sarteam.database.windows.net,1433;Initial Catalog=SARTeamCore;Persist Security Info=False;User ID=API;Password=2caIz49u$d61r%7dC;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+			dynamic requestAuthenticatedUser = Newtonsoft.Json.JsonConvert.DeserializeObject("{ \"ActiveTeam\":null, \"TeamUser\":null, \"ProfilePhotoURL\":null, \"DisplayName\":\"Bradley Don Morris\", \"EmailAddress\":\"bradleydonmorris@hotmail.com\", \"DefaultTeam\":null, \"Teams\":null, \"Profile\":null, \"KeyGUID\":\"00000000-0000-0000-0000-000000000000\" } ");
+			using SqlCommand sqlCommand = new();
+			String t = sqlCommand.ExecuteScalarString(
+				$"[Users].[Authenticate]",
+				connectionString,
+				new SqlParameter
 				{
-					ClassName = typeof(Program).FullName,
-					Name = System.Reflection.MethodBase.GetCurrentMethod()?.Name
+					ParameterName = "AuthenticatedUserJSON",
+					SqlDbType = SqlDbType.NVarChar,
+					Size = (-1),
+					Value = Newtonsoft.Json.JsonConvert.SerializeObject(requestAuthenticatedUser)
 				}
-				);
-			}
+			);
 
-
-			String connectionString = "Server=localhost;Initial Catalog=BDMSQLUtilities;User Id=BDMSQLUtilities.WebAPI;Password=213SDfg34sdabghfd&##dsfgsdfg^;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"BDMSQLUtilities.WebAPI\";";
-
-			using (SqlCommand sqlCommand = new())
-			{
-				var sdfasdf = sqlCommand.ExecuteScalarJsonProc<List<String>>(
-					$"[Optimization].[GetMissingIndexStatistics.json]",
-					connectionString,
-					new SqlParameter() { ParameterName = "@DatabaseName", SqlDbType = SqlDbType.NVarChar, Size = 128, SqlValue = "TrashDB", Direction = ParameterDirection.Input }
-				);
-			}
-			Console.WriteLine("Hello World!");
 		}
 	}
 }
